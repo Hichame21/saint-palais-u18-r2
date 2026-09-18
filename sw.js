0d547f63b81e2b9bfcf3e586b3b8f06abdc0821b
Hichame21 <hichame.kebbache@orange.fr>
2026-09-18 11:49:45 +0200

    Actualise le cache PWA pour la version cloud

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spm-u18-r2-v1-27-mobile-20260918';
+const CACHE='spm-u18-r2-v1-27-cloud-20260918-2';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
