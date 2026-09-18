163b82d12bcc541beba00769573b0b2a97055bd3
Hichame21 <hichame.kebbache@orange.fr>
2026-09-18 12:37:23 +0200

    Actualise le cache PWA pour la V1.28

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE='spm-u18-r2-v1-27-cloud-20260918-2';
+const CACHE='spm-u18-r2-v1-28-20260918';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET' || new URL(event.request.url).origin!==location.origin) return;
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
   if(event.request.mode==='navigate'){
     event.respondWith(fetch(event.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;}).catch(()=>caches.match('./index.html')));
     return;
