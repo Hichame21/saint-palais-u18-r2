8e1a010682cbea3d55b4ccc2c26960e41a4c90c1
Hichame21 <hichame.kebbache@orange.fr>
2026-09-18 14:32:23 +0200

    Force la mise à jour PWA pour le partage JPG

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spm-u18-r2-v1-28-20260918';
+const CACHE='spm-u18-r2-v1-28-share-jpg-20260918';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
