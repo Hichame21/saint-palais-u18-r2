d346fba8cb890fd342459714ee8e09e2232da9f0
Hichame21 <hichame.kebbache@orange.fr>
2026-09-20 21:19:45 +0200

    Actualise le cache PWA pour les points championnat

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spm-u18-r2-v1-28-history-lineup-20260918';
+const CACHE='spm-u18-r2-v1-28-championship-points-20260920';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
