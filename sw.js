34cdb4aa8a881d20f5585741e50448a54e4e2030
Hichame21 <hichame.kebbache@orange.fr>
2026-09-21 15:37:34 +0200

    Force le rechargement après correction V1.29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spm-u18-r2-v1-29-20260921';
+const CACHE='spm-u18-r2-v1-29-hotfix-20260921';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
