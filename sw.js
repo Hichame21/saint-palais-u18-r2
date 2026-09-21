ea0045c4f7d4951d59f583058139b610edd39271
Hichame21 <hichame.kebbache@orange.fr>
2026-09-21 15:58:06 +0200

    Renforce la mise à jour du service worker en V1.29.1

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-const CACHE='spm-u18-r2-v1-29-convoc-hotfix2-20260921';
+const CACHE='spm-u18-r2-v1-29-1-20260921';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
   if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;}).catch(()=>caches.match('./index.html')));
+    event.respondWith(fetch(event.request,{cache:'reload'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;}).catch(()=>caches.match('./index.html')));
     return;
   }
   event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r;}).catch(()=>caches.match(event.request)));
 });
+self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting();});
